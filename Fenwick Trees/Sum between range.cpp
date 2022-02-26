ff4301c4db0e619b59106f2e8f9d699101533d0f
Tripti Chaudhary <56844784+triptic1412@users.noreply.github.com>
2022-02-26 18:21:13 +0530

    Update Sum between range.cpp

--- Fenwick Trees/Sum between range.cpp
+++ Fenwick Trees/Sum between range.cpp
@@ -1,70 +1,69 @@
+/*
+
+	Type 0 - sum from l to r (both inclusive)
+	Type 1 - update arr[idx] by adding value x to it
+	Type 2 - update arr[idx] by making it x
+	
+*/
 #include<iostream>
 
 using namespace std;
 
-void update(int *arr, int *BIT, int i, int val, int n){
+void update(int *bit, int i, int val, int n){
 
-	for( ; i <= n ; i += (i & (-i)) ){
+		for( ; i <= n ; i += (i&(-i)) ){
 
-		BIT[i] += val;
+			bit[i] += val;
 
-	}
-}
-
-int query(int *BIT, int i){
-
-	int sum = 0;
-
-	for( ; i > 0 ; i -= (i & (-i)) ){
-
-		sum += BIT[i];
-
+		}
 	}
 
-	return sum;
-}
+	int sum(int *bit, int i){
+		
+		int ans = 0;
+
+		for( ; i > 0 ; i -= (i&(-i)) ){
+			ans += bit[i];
+		}
+
+		return ans;
+	}
+	int query(int *bit, int l, int r){
+
+		return (sum(bit,r) - sum(bit,l-1));
+
+	}
 int main(){
-
 	int n;
 	cin>>n;
 
 	int *arr = new int[n+1];
-	int *BIT = new int[n+1];
+	int *bit = new int[n+1];
 
-	for( int i = 1 ; i <= n ; i++ ){
-		arr[i] = 0;
-		BIT[i] = 0;
+	for(int i=0;i<=n;i++){
+		bit[i] = 0;
 	}
 
-	for( int i = 1 ; i <= n ; i++ ){
+	for(int i=1;i<=n;i++){
 		cin>>arr[i];
-		update(arr,BIT,i,arr[i],n);
+
+		//updating/building fenwick tree
+		update(bit,i,arr[i],n);
 	}
 
-	int queries;
-	cin>>queries;
+	int q;
+	cin>>q;
+	for(int i=1;i<=q;i++){
+		int type, a, b;
+		cin>>type>>a>>b;
 
-	for( int i = 1 ; i <= queries ; i++ ){
-
-		int type;
-		cin>>type;
-
-		if(type==0){
-
-			int idx,val;
-			cin>>idx>>val;
-			update(arr,BIT,idx,val,n);
-      
+		if(!type){
+			cout<<query(bit,a,b)<<endl;
 		}
 		else{
-      
-			int l,r;
-			cin>>l>>r;
-			cout<<( query(BIT,r) - query(BIT,l-1) )<<endl;
-      
+			int val = (type==1) ? b : (b-arr[a]);
+			update(bit,a,val,n);
 		}
-		
 	}
-
 	return 0;
 }
